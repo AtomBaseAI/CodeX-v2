820b29dabd952e63f808cdd02c31f307be4fb655
AtomAI <154759182+AtomBaseAI@users.noreply.github.com>
2024-07-07 07:48:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const cors = require("cors");
 const routes = require("./routes/index");
 const { PORT } = require("./config/server-config");
@@ -12,4 +13,10 @@ const startServer = async () => {
   server.listen(PORT, () => console.log(`Server started at port ${PORT}`));
 };
 
-startServer();
+// If running locally, start the server
+if (process.env.IS_OFFLINE) {
+  startServer();
+}
+
+// Export the handler for AWS Lambda
+module.exports.handler = serverless(server);
