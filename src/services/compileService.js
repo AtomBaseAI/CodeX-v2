9622eede7082d7348f87cd96336a1f576a97c189
AtomAI <154759182+AtomBaseAI@users.noreply.github.com>
2024-07-07 08:24:13 +0530

    Update compileService.js

--- src/services/compileService.js
+++ src/services/compileService.js
@@ -1,24 +1,10 @@
+const runCode = require("./code-runner");
 const createFiles = require("../file-system/createFile");
-const random = require("../utils/random");
 
 class CompileService {
-  async runCode(req) {
-    try {
-      const data = {
-        src: req.body.src,
-        stdin: req.body.stdin,
-        lang: req.body.lang,
-        filename: "Test" + random(10),
-      };
-      if (data.src && data.lang) {
-        return await createFiles(data);
-      } else {
-        return { output: "Invalid Request", status: "Invalid Request" };
-      }
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+  async runCode(json_msg) {
+    const result = await createFiles(json_msg);
+    return result;
   }
 }
 
