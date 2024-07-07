3440979a4b5bec4c5eea2958808d876dd4d4d779
AtomAI <154759182+AtomBaseAI@users.noreply.github.com>
2024-07-07 08:23:09 +0530

    Update deleteFile.js

--- src/file-system/deleteFile.js
+++ src/file-system/deleteFile.js
@@ -2,32 +2,6 @@ const fs = require("fs").promises;
 const path = require("path");
 const os = require("os");
 
-const extensions = {
-  cpp: "cpp",
-  c: "c",
-  java: "java",
-  python3: "py",
-  javascript: "js",
-};
-
-async function createFiles(json_msg) {
-  const tempDir = os.tmpdir();
-  const filePath = path.join(tempDir, `${json_msg.filename}.${extensions[json_msg.lang]}`);
-
-  try {
-    await fs.writeFile(filePath, json_msg.src);
-    console.log("Source file created");
-    const ans = await runCode({ ...json_msg, filePath });
-    return ans;
-  } catch (error) {
-    console.log(error);
-    await deletingTempFiles(tempDir);
-    throw error;
-  }
-}
-
-module.exports = createFiles;
-
 async function deletingTempFiles(tempDir) {
   try {
     const files = await fs.readdir(tempDir);
