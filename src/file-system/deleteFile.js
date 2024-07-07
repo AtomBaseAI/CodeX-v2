8884a8e0dd0a63be972f8dc3661f11d9021e6da7
AtomAI <154759182+AtomBaseAI@users.noreply.github.com>
2024-07-07 08:12:52 +0530

    Update deleteFile.js

--- src/file-system/deleteFile.js
+++ src/file-system/deleteFile.js
@@ -1,12 +1,42 @@
-const fs = require("fs/promises");
+const fs = require("fs").promises;
 const path = require("path");
+const os = require("os");
 
-async function deletingTempFiles() {
+const extensions = {
+  cpp: "cpp",
+  c: "c",
+  java: "java",
+  python3: "py",
+  javascript: "js",
+};
+
+async function createFiles(json_msg) {
+  const tempDir = os.tmpdir();
+  const filePath = path.join(tempDir, `${json_msg.filename}.${extensions[json_msg.lang]}`);
+
   try {
-    const files = await fs.readdir(path.join(process.cwd(), "/temp/"));
-    await Promise.all(files.map(file => fs.unlink(path.join(process.cwd(), `/temp/${file}`))));
+    await fs.writeFile(filePath, json_msg.src);
+    console.log("Source file created");
+    const ans = await runCode({ ...json_msg, filePath });
+    return ans;
   } catch (error) {
-    console.error(error);
+    console.log(error);
+    await deletingTempFiles(tempDir);
+    throw error;
+  }
+}
+
+module.exports = createFiles;
+
+async function deletingTempFiles(tempDir) {
+  try {
+    const files = await fs.readdir(tempDir);
+    for (const file of files) {
+      await fs.unlink(path.join(tempDir, file));
+      console.log("DELETED file -> " + file);
+    }
+  } catch (error) {
+    console.log(error);
     throw error;
   }
 }
